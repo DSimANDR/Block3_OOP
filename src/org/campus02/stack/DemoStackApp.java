fefb1ea13766695d130c6830a731f41eda76b64b
skochauf <stephan.kochauf@lv.campus02.at>
2021-11-06 13:28:53 +0100

    StackDemo

--- src/org/campus02/stack/DemoStackApp.java
+++ src/org/campus02/stack/DemoStackApp.java
@@ -1,4 +1,21 @@
 package org.campus02.stack;
 
 public class DemoStackApp {
+    public static void main(String[] args) {
+
+        Stack stapel = new Stack(new int[7]);
+
+        stapel.push(7);
+        stapel.push(5);
+        stapel.push(6);
+
+        System.out.println("stapel.pop() = " + stapel.pop());
+        System.out.println("stapel.pop() = " + stapel.pop());
+        stapel.push(3);
+        System.out.println("stapel.pop() = " + stapel.pop());
+        System.out.println("stapel.pop() = " + stapel.pop());
+        System.out.println("stapel.pop() = " + stapel.pop());
+
+
+    }
 }
